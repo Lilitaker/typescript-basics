4ceeafaca85ea8cd4c21a12f4e9dfaf12174d6ee
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2023-02-15 08:30:28 -0500

    Use date-fns library that supports typescript

---
+++ src/13-date-fns.ts
@@ -0,0 +1,8 @@
+//Using the JS date-fns library - it supports TS
+
+import { subDays, format } from 'date-fns';
+
+const date = new Date(1998, 1, 28); // 0 = enero / 1 = febrero
+const rta = subDays(date, 30); //Restar 30 días a la fecha
+const string = format(rta, 'yyyy/MM/dd');
+console.log(string); //1998/01/29
